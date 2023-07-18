25a96cb1f716fdfe5451c74a2b270b7d14083c53
s7and <7stand7@gmail.com>
2023-07-19 01:03:09 +0200

    add Translator class

--- backend/ClipboardTranslate.py
+++ backend/ClipboardTranslate.py
@@ -1,15 +1,11 @@
-from threading import Thread
-from time import sleep
-from PIL import Image, ImageGrab
-from googletrans import Translator
-import pytesseract
-from colorama import Fore
-from flask import Flask
+#!/usr/bin/python3
 
-custom_conf = r'--psm 11'
-translator = Translator()
-app = Flask( __name__ )
-texts = []
+from flask import Flask
+import argparse
+
+from Translator import Translator
+
+app = Flask(__name__)
 
 @app.route("/get_new_text")
 def hello():
@@ -19,41 +15,24 @@ def hello():
         return texts.pop(0)
     return ""
 
-
-def translate_image(image):
-    global texts
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-    result = pytesseract.image_to_string(image, config=custom_conf, output_type='string')
-    result = result.replace('\n', ' ')
-    print(Fore.RED + '--en--', flush=True)
-    print(Fore.RED + result, flush=True)
-    translate = translator.translate(result, dest='ru')
-    print(Fore.GREEN + '--ru--', flush=True)
-    print(Fore.GREEN + translate.text, flush=True)
-    texts.append(result)
-    texts.append(translate.text)
+def getParameters():
+    parser = argparse.ArgumentParser(
+                    prog='Clipboard Transate',
+                    description='Get screenshot from clipboard and translates it.')
+    parser.add_argument('-p', '--tesseract_path', default='C:\\Program Files\\Tesseract-OCR\\tesseract.exe')
+    parser.add_argument('--host', default='127.0.0.1')
+    parser.add_argument('--port', default=5000)
+    args = parser.parse_args()
+    return args
 
 
-def try_save_image(image):
-    try:
-        print('try_save_image', flush=True)
-        cropped_image = image.crop((250, 770, 1600, 1000))
-        translate_image(cropped_image)
-    except Exception as e:
-        print('Ошибка при сохранении изображения:', e)
+def main():
+    args = getParameters()
+    translator = Translator(args.tesseract_path)
 
-def screen_push():
-    last_clipboard_image = ImageGrab.grabclipboard()
-    while True:
-        print('thread 2', flush=True)
-        clipboard_image = ImageGrab.grabclipboard()
-        if clipboard_image is not None and clipboard_image != last_clipboard_image:
-            try_save_image(clipboard_image)
-            last_clipboard_image = clipboard_image
-        sleep(1)
+    app.run(host=args.host, port=args.port, debug=True, threaded=True)
+    translator.stop()
 
-thread = Thread(target=screen_push)
-thread.daemon = True
-thread.start()
 
-app.run( host = "127.0.0.1", port = 5000, debug=True, threaded=True)
+if __name__ == '__main__':
+    main()
---
+++ backend/Translator.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+from colorama import Fore
+from googletrans import Translator
+from PIL import ImageGrab
+import pytesseract
+from threading import Thread
+from time import sleep
+
+class Translator(object):
+    def __init__(self, tesseract_path='heuasodjaosdj'):
+        self._tesseract_path = tesseract_path
+        self._texts = []
+        self._custom_conf = r'--psm 11'
+        self._thread = Thread(target=self.screen_push)
+        self._thread.daemon = True
+        self._thread.start()
+        self._is_running = True
+
+    
+    def stop(self):
+        self._is_running = False
+        self._thread.join()
+
+    def translate_image(self, image):
+        global texts
+        pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        result = pytesseract.image_to_string(
+            image, config=self._custom_conf, output_type='string')
+        result = result.replace('\n', ' ')
+        print(Fore.RED + '--en--', flush=True)
+        print(Fore.RED + result, flush=True)
+        translate = self._translator.translate(result, dest='ru')
+        print(Fore.GREEN + '--ru--', flush=True)
+        print(Fore.GREEN + translate.text, flush=True)
+        texts.append(result)
+        texts.append(translate.text)
+
+
+    def try_save_image(self, image):
+        try:
+            print('try_save_image', flush=True)
+            cropped_image = image.crop((250, 770, 1600, 1000))
+            self.translate_image(cropped_image)
+        except Exception as e:
+            print('Ошибка при сохранении изображения:', e)
+
+
+    def screen_push(self):
+        last_clipboard_image = ImageGrab.grabclipboard()
+        while self._is_running:
+            print('thread 2', flush=True)
+            clipboard_image = ImageGrab.grabclipboard()
+            if clipboard_image is not None and clipboard_image != last_clipboard_image:
+                self.try_save_image(clipboard_image)
+                last_clipboard_image = clipboard_image
+            sleep(1)
