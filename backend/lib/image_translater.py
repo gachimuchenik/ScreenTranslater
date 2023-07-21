481874c51b2933dcc03008f9a0c3d06de0e74caa
Denis <this.denis@gmail.com>
2023-07-21 06:00:20 +0400

    added pattern analyzer

--- backend/lib/image_translater.py
+++ backend/lib/image_translater.py
@@ -3,9 +3,11 @@
 
 import time
 import functools
+import csv
 
 import pytesseract
 from googletrans import Translator as GoogleTranslator
+from lib.area_pattern_analyzer import AreaPatternAnalyzer
 
 
 class ImageTranslater(object):
@@ -58,6 +60,24 @@ class ImageTranslater(object):
         input_text = input_text.replace('\n', ' ')
         return input_text
 
+    # in: image
+    # out: text
+    @log_and_calc
+    def _recognize_text_from_tesseract_data(self, image):
+        pytesseract.pytesseract.tesseract_cmd = self._config.tesseract_path
+        data_csv = pytesseract.image_to_data(image, config='--psm 3', output_type='string')
+        data_csv_lines = data_csv.splitlines()
+        csv_reader = csv.reader(data_csv_lines, delimiter='\t')
+        csv_reader.__next__() # skip head row
+        input_text = ''
+        for row in csv_reader:
+            confidence = float(row[10])
+            text = row[11]
+            if confidence > 80:
+                input_text += text + ' '
+        input_text = input_text.replace('| ', 'I ')
+        return input_text
+
     @log_and_calc
     def _translate_text(self, text):
         """ Translate text
@@ -86,3 +106,9 @@ class ImageTranslater(object):
         result = image.convert('L').point(
             lambda x: 255 if x > self._config.grayscale_threshold else 0).convert('RGB')
         return result
+
+    @log_and_calc
+    def _pattern_analysis(self, image):
+        test = AreaPatternAnalyzer(self._config)
+        self._log.info(test.pattern_analysis(image))
+        return image
